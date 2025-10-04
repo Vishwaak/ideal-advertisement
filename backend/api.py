63195087a50903c5909890d459176e14ab18ad73
vishwaak <chandranvishwaak@gmail.com>
2025-10-04 19:55:59 -0400

    feat:emotional score

--- backend/api.py
+++ backend/api.py
@@ -2,27 +2,47 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import UploadFile, File
 from typing import List
+from main import Context_engine
 
 app = FastAPI()
+
+context_engine = Context_engine()
 
 
 # Define a response model
 class OutputData(BaseModel):
-    result: str
+    result: dict
+
 
 @app.post("/ad_placement", response_model=OutputData)
-async def upload_video(file: UploadFile = File(...)):
+async def upload_video(file: UploadFile = File(None), video_id: str = ""):
     # Mock function to simulate video processing
     def process_video(file: UploadFile) -> List[dict]:
         # Replace this with actual video processing logic
-        return [
-            {"timestamp": "00:00:10", "description": "Scene 1 description"},
-            {"timestamp": "00:00:20", "description": "Scene 2 description"},
-        ]
+        video_id = context_engine.upload_vid(file.file)
+        result = context_engine.call_pegasus(
+            video_id,
+            "chapterize the video for emotion timeline and time stamp it based on the video",
+        )
 
-    # Process the uploaded video file
-    result = process_video(file)
-    return {"result": str(result)}
+        return result
+
+    if file:
+        # Process the uploaded video file
+        result = process_video(file)
+
+        return {"result": result}
+
+    elif video_id:
+        result = context_engine.call_pegasus(
+            video_id,
+            "chapterize the video for emotion timeline and time stamp it based on the video",
+        )
+        return {"result": {"data": result}}
+
+    else:
+        return {"result": "No file or video_id provided. Either provide one of them"}
+
 
 @app.get("/")
 async def root():
