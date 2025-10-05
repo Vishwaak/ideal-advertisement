3054419c790d3acf40fe0b722e566cbcb0e1e78a
vishwaak <chandranvishwaak@gmail.com>
2025-10-05 10:34:42 -0400

    chore: fixed the plot_function

--- backend/main.py
+++ backend/main.py
@@ -66,13 +66,15 @@ class Context_engine:
 
         with open(csv_file_path, mode="r") as csv_file:
             csv_reader = csv.reader(csv_file)
+            count =0
             for row in csv_reader:
-                if len(row) == 2:  # Ensure the row has two elements (timestamp and emotion value)
-                    try:
-                        timestamps.append(row[0])
-                        emotions.append(float(row[1]))
-                    except ValueError:
-                        self.logger.warning(f"Skipping invalid emotion value: {row[1]}")
+                if count > 2:
+                    if len(row) == 2:  # Ensure the row has two elements (timestamp and emotion value)
+                        try:
+                            timestamps.append(row[0])
+                            emotions.append(float(row[1]))
+                        except ValueError:
+                            self.logger.warning(f"Skipping invalid emotion value: {row[1]}")
 
         # Plot the data
         plt.figure(figsize=(10, 6))
@@ -213,40 +215,40 @@ class Context_engine:
 
         return json.loads(text_steam.data)
 
-if __name__ == "__main__":
-    context_engine = Context_engine()
-    data = {
-  "result": {
-    "data": {
-      "timestamps": [
-        {
-          "description": "Preparation and Tension, building up as the play is about to begin.",
-          "time": "0s (00:00)"
-        },
-        {
-          "description": "Execution of the Play, excitement as Barkley makes a significant run.",
-          "time": "4s (00:04)"
-        },
-        {
-          "description": "Successful Run, satisfaction and relief as Barkley gains substantial yardage.",
-          "time": "12s (00:12)"
-        },
-        {
-          "description": "Celebration, joy and triumph as Barkley celebrates with his teammates.",
-          "time": "17s (00:17)"
-        },
-        {
-          "description": "Replay and Reflection, appreciation and admiration as the play is shown from different angles.",
-          "time": "26s (00:26)"
-        }
-      ]
-    }
-  }
-}
+# if __name__ == "__main__":
+#     context_engine = Context_engine()
+#     data = {
+#   "result": {
+#     "data": {
+#       "timestamps": [
+#         {
+#           "description": "Preparation and Tension, building up as the play is about to begin.",
+#           "time": "0s (00:00)"
+#         },
+#         {
+#           "description": "Execution of the Play, excitement as Barkley makes a significant run.",
+#           "time": "4s (00:04)"
+#         },
+#         {
+#           "description": "Successful Run, satisfaction and relief as Barkley gains substantial yardage.",
+#           "time": "12s (00:12)"
+#         },
+#         {
+#           "description": "Celebration, joy and triumph as Barkley celebrates with his teammates.",
+#           "time": "17s (00:17)"
+#         },
+#         {
+#           "description": "Replay and Reflection, appreciation and admiration as the play is shown from different angles.",
+#           "time": "26s (00:26)"
+#         }
+#       ]
+#     }
+#   }
+# }
     
-    data = {"timestamps":[{"description":"The initial chase creates tension as the player in black attempts to catch up with the player in red.","time":"0s (00:00) - 4s (00:04)"},{"description":"As the player in red skillfully maneuvers past the defender, excitement and thrill are evident.","time":"4s (00:04) - 8s (00:08)"},{"description":"The climax occurs when the player in red scores a goal, evoking joy and triumph.","time":"8s (00:08) - 11s (00:11)"},{"description":"The celebration phase begins, marked by the player's body language and the crowd's response, filled with happiness and satisfaction.","time":"11s (00:11) - 13s (00:13)"}]}
-    context_engine.call_gemini(data, prompt_type="emotion")
-#     vid_id = '68e17ba917b39f617835d2da'
-#     query = "chapterize the video for emotion timeline and time stamp it based on the video"
-#     response = context_engine.call_pegasus(vid_id, query)
-#     print("Response from Marengo:", response)
+#     data = {"timestamps":[{"description":"The initial chase creates tension as the player in black attempts to catch up with the player in red.","time":"0s (00:00) - 4s (00:04)"},{"description":"As the player in red skillfully maneuvers past the defender, excitement and thrill are evident.","time":"4s (00:04) - 8s (00:08)"},{"description":"The climax occurs when the player in red scores a goal, evoking joy and triumph.","time":"8s (00:08) - 11s (00:11)"},{"description":"The celebration phase begins, marked by the player's body language and the crowd's response, filled with happiness and satisfaction.","time":"11s (00:11) - 13s (00:13)"}]}
+#     context_engine.call_gemini(data, prompt_type="emotion")
+# #     vid_id = '68e17ba917b39f617835d2da'
+# #     query = "chapterize the video for emotion timeline and time stamp it based on the video"
+# #     response = context_engine.call_pegasus(vid_id, query)
+# #     print("Response from Marengo:", response)
